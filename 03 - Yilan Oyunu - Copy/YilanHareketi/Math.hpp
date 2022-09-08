43fedf4cb63430e0556a96fb31882181150fef14
kayhanayar <kayhanayar@gmail.com>
2022-09-08 03:50:27 -0700

    yeni

--- 03 - Yilan Oyunu - Copy/YilanHareketi/Math.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-float dot(const sf::Vector2f& sol, const sf::Vector2f& sag);
-
-float uzunluk(const sf::Vector2f& vec);
-
-sf::Vector2f normalize(const sf::Vector2f& vec);
-
- float RadToDeg(float Rad);
--- 03 - Yilan Oyunu - Copy/baslangic/Cizilebilir.cpp
+++
@@ -1,6 +0,0 @@
-#include "Cizilebilir.hpp"
-
-sf::Vector2f Cizilebilir::konumGetir()
-{
-	return m_konum;
-}
